12ae96c4dff73bc7b05dd26a8533aa17864ca3a2
Janik von Rotz <login@janikvonrotz.ch>
2021-01-04 11:54:55 +0100

    edit on github and last updated

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,13 +2,17 @@ module.exports = {
     title: 'python.casa',
     description: 'Einführung in die Programmierung.',
     themeConfig: {
-        sidebar: 'auto'
+        sidebar: 'auto',
+        repo: 'janikvonrotz/python.casa',
+        docsBranch: 'main',
+        editLinks: true
     },
     plugins: [
         '@vuepress/active-header-links',
         '@vuepress/medium-zoom',
         'vuepress-plugin-mermaidjs',
         'plausible-analytics',
-        '@vuepress/back-to-top'
+        '@vuepress/back-to-top',
+        '@vuepress/last-updated'
     ]
 }
